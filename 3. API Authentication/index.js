f2ab984e0e6778c3bb4c497df26290553efdc016
devesh1808 <devesh1808@gmail.com>
2024-12-21 17:14:01 +0530

    did api key authorisation using axios

--- 3. API Authentication/index.js
+++ 3. API Authentication/index.js
@@ -37,10 +37,18 @@ app.get("/basicAuth", async (req, res) => {
   }
 });
 
-app.get("/apiKey", (req, res) => {
-  //TODO 4: Write your code here to hit up the /filter endpoint
-  //Filter for all secrets with an embarassment score of 5 or greater
-  //HINT: You need to provide a query parameter of apiKey in the request.
+app.get("/apiKey", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/filter", {
+      params: {
+        score: 5,
+        apiKey: yourAPIKey,
+      },
+    });
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 });
 
 app.get("/bearerToken", (req, res) => {
