59a7e8a768cf279b6cd776107605b0d85940a6a1
devesh1808 <devesh1808@gmail.com>
2024-12-23 13:00:08 +0530

    made the get request using axios to fetch the data from secrets api and passed the api data to index.ejs file to render on home page

--- 5. Secrets Project/index.js
+++ 5. Secrets Project/index.js
@@ -4,12 +4,19 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com";
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// 5. Use axios to get a random secret and pass it to index.ejs to display the
-// secret and the username of the secret.
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(API_URL + "/random");
+    res.render("index.ejs", { data: response.data });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
